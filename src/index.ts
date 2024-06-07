e6a156ff90025d829c742b6a665acedd54c6ab4b
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2024-06-07 17:46:47 -0400

    step 6: execute command with arguments

--- src/index.ts
+++ src/index.ts
@@ -27,12 +27,13 @@ function _activate(app: JupyterFrontEnd,
   let commandId = 'epjlx:Hello';
   app.commands.addCommand(commandId,
     { label: 'Hello World',
-      execute: () => { say_hello(app) }
+      execute: (args: any) => { say_hello(app, args) }
     });
 
   palette.addItem(
     { command: commandId,
-      category: 'Anything'
+      category: 'Anything',
+      args: {greeting: 'Hello from the Command Palette!'}
     });
 
   if (launcher) {
@@ -47,7 +48,7 @@ function _activate(app: JupyterFrontEnd,
 }
 
 class HelloWorldWidget extends Widget {
-  constructor() {
+  constructor(greeting: string|null) {
     super();
     this.id = 'hello-world';
     this.title.label = 'Hello World';
@@ -55,14 +56,18 @@ class HelloWorldWidget extends Widget {
     this.addClass('hww');
     let body = document.createElement('body');
     let heading = document.createElement('h1');
-    heading.innerText = 'Hello World from EuroPython!';
+    if (greeting) {
+      heading.innerText = greeting;
+    } else {
+      heading.innerText = 'Hello World from EuroPython!';
+    }
     body.appendChild(heading);
     this.node.appendChild(body);
   }
 }
 
-function say_hello(app: JupyterFrontEnd) {
-  let content = new HelloWorldWidget();
+function say_hello(app: JupyterFrontEnd, args: any) {
+  let content = new HelloWorldWidget(args['greeting']);
   let widget  = new MainAreaWidget({content});
 
   app.shell.add(widget,'main');
